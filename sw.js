0546505ceb9314dda4af94f474567ec82edb9af0
LuWu187 <lucas@wurster.me>
2026-08-10 10:03:08 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'matchday-prep-v1';
+const CACHE_NAME = 'rekomp-v2';
 const SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
